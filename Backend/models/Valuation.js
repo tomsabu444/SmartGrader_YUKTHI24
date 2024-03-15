aa21c5bd6f5238bc25f34a1b694a3dec375cd4f2
Tom Sabu <tomsabu0000@gmail.com>
2024-03-15 20:11:42 +0530

    database model schema

---
+++ Backend/models/Valuation.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const ValuationSchema = new mongoose.Schema(
+    {
+        valuatorId: {
+            type: String,
+            required: true
+        },
+        data: {
+            type: Object,
+            required: true
+        },
+        answerSheet: {
+            type: String,
+            required: true
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Valuation = mongoose.model("Valuation", ValuationSchema);
+
+export default Valuation;
